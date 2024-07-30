07dd198e47fffb0bbd2cc646de14ae6cd21db9b0
cherisphine <odhiambcherisphine@gmail.com>
2024-07-30 21:27:56 +0300

    Initial SMS

--- SMS/settings.py
+++ SMS/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'SMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_management_system',
-        'USER': 'root',
-        'PASSWORD': 'N@ll$nKunashe17',
-        'HOST': 'localhost', 
-        'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'student_management_system',
+         #'USER': 'root',
+        # 'PASSWORD': 'N@ll$nKunashe17',
+         #'HOST': 'localhost', 
+         #'PORT': '3306',
 
     }
 }
